c72de92951a81a5b7414bbeb892257707b0c2d72
Sridhar Ratnakumar <srid@srid.ca>
2023-09-25 12:25:12 -0400

    wip

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Your jupyenv project";
+
+  nixConfig.extra-substituters = [
+    "https://tweag-jupyter.cachix.org"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "tweag-jupyter.cachix.org-1:UtNH4Zs6hVUFpFBTLaA4ejYavPo5EFFqgd7G7FxGW9g="
+  ];
+
+  inputs.flake-compat.url = "github:edolstra/flake-compat";
+  inputs.flake-compat.flake = false;
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.jupyenv.url = "github:juspay/jupyenv/macos";
+
+  outputs = {
+    self,
+    flake-compat,
+    flake-utils,
+    nixpkgs,
+    jupyenv,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachSystem
+    [
+      flake-utils.lib.system.aarch64-darwin
+    ]
+    (
+      system: let
+        inherit (jupyenv.lib.${system}) mkJupyterlabNew;
+        jupyterlab = mkJupyterlabNew ({...}: {
+          nixpkgs = inputs.nixpkgs;
+          imports = [(import ./kernels.nix)];
+        });
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        #packages = {inherit jupyterlab;};
+        #packages.tweag = jupyterlab;
+        packages.default  = pkgs.python3.withPackages(ps: with ps; [
+           numpy pandas jupyter 
+           nbdev 
+           matplotlib
+           ]);
+        apps.default.program = "${packages.default}/bin/jupyter-lab";
+        apps.default.type = "app";
+      }
+    );
+}
