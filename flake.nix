dc890dee33cce819f2504bcdcbce7ffb77bf709e
Sridhar Ratnakumar <srid@srid.ca>
2023-09-25 12:42:20 -0400

    working

--- flake.nix
+++ flake.nix
@@ -1,49 +1,35 @@
 {
-  description = "Your jupyenv project";
-
-  nixConfig.extra-substituters = [
-    "https://tweag-jupyter.cachix.org"
-  ];
-  nixConfig.extra-trusted-public-keys = [
-    "tweag-jupyter.cachix.org-1:UtNH4Zs6hVUFpFBTLaA4ejYavPo5EFFqgd7G7FxGW9g="
-  ];
-
-  inputs.flake-compat.url = "github:edolstra/flake-compat";
-  inputs.flake-compat.flake = false;
+  description = "fastbook";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.jupyenv.url = "github:juspay/jupyenv/macos";
 
-  outputs = {
-    self,
-    flake-compat,
-    flake-utils,
-    nixpkgs,
-    jupyenv,
-    ...
-  } @ inputs:
+  outputs = { self, flake-utils, nixpkgs, ... } @ inputs:
     flake-utils.lib.eachSystem
-    [
-      flake-utils.lib.system.aarch64-darwin
-    ]
-    (
-      system: let
-        inherit (jupyenv.lib.${system}) mkJupyterlabNew;
-        jupyterlab = mkJupyterlabNew ({...}: {
-          nixpkgs = inputs.nixpkgs;
-          imports = [(import ./kernels.nix)];
-        });
-        pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        #packages = {inherit jupyterlab;};
-        #packages.tweag = jupyterlab;
-        packages.default  = pkgs.python3.withPackages(ps: with ps; [
-           numpy pandas jupyter 
-           nbdev 
-           matplotlib
-           ]);
-        apps.default.program = "${packages.default}/bin/jupyter-lab";
-        apps.default.type = "app";
-      }
-    );
+      [
+        flake-utils.lib.system.aarch64-darwin
+        flake-utils.lib.system.x86_64-linux
+      ]
+      (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          formatter = pkgs.nixpkgs-fmt;
+          packages.default = pkgs.python3.withPackages (ps: with ps; [
+            pandas
+            jupyter
+            nbdev
+            matplotlib
+            scikit-learn
+            graphviz
+            fastai
+            sentencepiece
+            ipywidgets
+            # azure-cognitiveservices-search-imagesearch
+          ]);
+          apps.default.program = "${packages.default}/bin/jupyter-lab";
+          apps.default.type = "app";
+        }
+      );
 }
